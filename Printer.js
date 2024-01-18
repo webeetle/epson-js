68335be3e5c5bf9caebbafa3289b308fc4d611e0
davide <davidecpr98@gmail.com>
2024-01-18 11:42:38 +0100

    stato stampante

--- Printer.js
+++ Printer.js
@@ -1,32 +1,39 @@
-const {ePOSBuilder, CanvasPrint, encodeBase64Binary, ePOSPrint, fiscalPrint} = require('./js/fiscalprint')
+const {ePOSBuilder, encodeBase64Binary, fiscalPrint} = require('./js/fiscalprint');
+const {XMLParser, XMLBuilder} = require('fast-xml-parser');
 
 class Printer {
 
     url = ""
+    printer = null
 
     constructor(ipAddress) {
         this.url = `${window.location.protocol}//${ipAddress}/cgi-bin/fpmate.cgi`;
-        this.printer = new fiscalPrint(); // Assumiendo che ePOSPrint sia una classe per gestire la stampante.
+        this.printer = new fiscalPrint();
     }
 
-    // Metodo per inviare dati XML alla stampante
-    sendToPrinter(xmlData) {
+    sendToPrinter(xmlData, onError = null, onReceive = null) {
         if (!this._isValidXml(xmlData)) {
             throw new Error("Dati XML non validi.");
         }
 
         this.printer.onerror = function (result) {
             console.error("[PRINTER] Errore durante la stampa: " + result);
+            if (onError) onError(result);
         }
 
         this.printer.onreceive = function (result, tag_names_array, add_info, res_add) {
             console.log("[PRINTER] ", result, tag_names_array, add_info, res_add);
+            if (onReceive) onReceive(result, tag_names_array, add_info, res_add);
         }
 
         this.printer.send(this.url, xmlData, 0);
     }
 
-    // Metodo privato per validare i dati XML
+    getPrinterStatus() {
+        const xmlData = this._getPrinterStatusXml();
+        this.sendToPrinter(xmlData);
+    }
+
     _isValidXml(xmlData) {
         try {
             const parser = new DOMParser();
@@ -35,6 +42,20 @@ class Printer {
         } catch (e) {
             return false;
         }
+    }
+
+    _getPrinterStatusXml() {
+        const statusObj = {
+            "queryPrinterStatus": ""
+        }
+
+        const options = {
+            arrayNodeName: "printerCommand",
+            suppressEmptyNode: true
+        }
+
+        const builder = new XMLBuilder(options)
+        return builder.build(statusObj);
     }
 }
 
