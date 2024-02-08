78101ec6399e35d3a4d834bd723c2bc70781a5bf
davide <davidecpr98@gmail.com>
2024-02-08 17:43:09 +0100

    debug mode + reset printer

--- Printer.js
+++ Printer.js
@@ -7,40 +7,58 @@ class Printer {
     url = ""
     printer = null
 
-    constructor(ipAddress, protocol = "http") {
+    constructor(ipAddress, protocol = "http", debug = false) {
         this.url = `${protocol}://${ipAddress}/cgi-bin/fpmate.cgi`;
         this.printer = new fiscalPrint();
     }
 
-    sendToPrinter(xmlData, onError = null, onReceive = null) {
+    sendToPrinter(xmlData, onError = null, onReceive = null, resetPrinter = true, debug = false) {
         if (!this._isValidXml(xmlData)) {
             throw new Error("Dati XML non validi.");
         }
 
         this.printer.onerror = function (result) {
-            console.error("[PRINTER] Errore durante la stampa: " + result);
+            if (debug) console.error("[PRINTER] Errore durante la stampa: " + result);
             if (onError) onError(result);
         }
 
         this.printer.onreceive = function (result, tag_names_array, add_info, res_add) {
-            console.log("[PRINTER] ", result, tag_names_array, add_info, res_add);
+            if (debug) console.log("[PRINTER] ", result, tag_names_array, add_info, res_add);
             if (onReceive) onReceive(result, tag_names_array, add_info, res_add);
+
         }
 
         this.printer.send(this.url, xmlData, 10000);
     }
 
-    getPrinterStatus(onError = null, onReceive = null) {
+    resetPrinter(onError = null, onReceive = null, debug = false) {
+        const xmlData = this._getResetPrinterXml();
+
+        const onResetError = (result) => {
+            if (debug) console.log("[PRINTER][CALLBACK] Errore durante il reset della stampante: " + result);
+            const error = this._formatError(result);
+            if (onError) onError(error);
+        }
+
+        const onResetReceive = (result, tag_names_array, add_info, res_add) => {
+            if (debug) console.log("[PRINTER][CALLBACK] ", result, tag_names_array, add_info, res_add);
+            if (onReceive) onReceive(result, tag_names_array, add_info, res_add);
+        }
+
+        this.sendToPrinter(xmlData, onResetError, onResetReceive, false);
+    }
+
+    getPrinterStatus(onError = null, onReceive = null, debug = false) {
         const xmlData = this._getPrinterStatusXml();
 
         const onStatusError = (result) => {
-            console.log("[PRINTER][CALLBACK] Errore durante la stampa: " + result);
+            if (debug)  console.log("[PRINTER][CALLBACK] Errore durante la stampa: " + result);
             const error = this._formatError(result);
             if (onError) onError(error);
         }
 
         const onStatusReceive = (result, tag_names_array, add_info, res_add) => {
-            console.log("[PRINTER][CALLBACK] ", result, tag_names_array, add_info, res_add);
+            if (debug)  console.log("[PRINTER][CALLBACK] ", result, tag_names_array, add_info, res_add);
             const status = this._formatStatus(result, add_info);
             if (onReceive) onReceive(status);
         }
@@ -70,8 +88,22 @@ class Printer {
         }
 
         const builder = new XMLBuilder(options)
-        console.log("[PRINTER] XML Stato: ", builder.build(statusObj));
         return builder.build(statusObj);
+    }
+
+    _getResetPrinterXml() {
+        const resetObj = {
+            printerCommand: {
+                resetPrinter: ""
+            }
+        }
+
+        const options = {
+            suppressEmptyNode: true
+        }
+
+        const builder = new XMLBuilder(options)
+        return builder.build(resetObj);
     }
 
     _formatError(error) {
